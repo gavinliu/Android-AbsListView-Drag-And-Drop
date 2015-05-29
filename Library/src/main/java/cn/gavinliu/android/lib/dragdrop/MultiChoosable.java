9431357dce324e03c965cbce24fc22fb2e55821e
刘云龙 <gavin6liu@gmail.com>
2015-05-29 16:21:33 +0800

    update header transformer

---
+++ Library/src/main/java/cn/gavinliu/android/lib/dragdrop/MultiChoosable.java
@@ -0,0 +1,14 @@
+package cn.gavinliu.android.lib.dragdrop;
+
+/**
+ * Created by GavinLiu on 2015-05-29
+ */
+public interface MultiChoosable {
+
+    void multiChooseAll();
+
+    void clearMultiChoose();
+
+    void exitMultiChoiceMode();
+
+}
